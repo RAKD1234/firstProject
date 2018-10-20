a730793abeb9bb4d7be6e7dbc044290afeeea2e9
RAKD1234 <rickcg987@gmail.com>
2018-10-20 13:58:48 -0400

    add

---
+++ main.js
@@ -0,0 +1,24 @@
+console.log("hello world!");
+if (5 > 4) {
+
+    console.log("5 is gt 4");
+    }
+
+
+
+  
+    let userName= prompt("What's your name?");
+let con = confirm("Hey " + userName + " Want to have a mango?"); 
+
+if (con == false){
+let al = alert("sorry to know that yo don't like mangoes");
+}
+else
+alert("Great!Lets go");
+
+console.log("User's name is " + userName + "."); 
+
+
+ 
+
+
